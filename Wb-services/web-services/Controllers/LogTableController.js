b7da64669bad9b80b90127edeba24647eef4d1e4
Anurag Rawal <anuragr07@yahoo.com>
2023-04-09 20:06:08 -0700

    Log table done

--- Wb-services/web-services/Controllers/LogTableController.js
+++ Wb-services/web-services/Controllers/LogTableController.js
@@ -3,14 +3,34 @@ const LogTable = require("../Model/LogTable")
 // This will return latest 3 log enteries
 exports.getLatestLogTableEntries = async (req, res) => {
     try {
-        const username = req.params.username;
-        console.log(username);
-        const logTableEntries = await LogTable.find({ username: username })
-            .sort({ _id: -1 })
-            .limit(3);
-        
-        console.log(logTableEntries);
-        res.status(200).json(logTableEntries);
+        const totalConsumption = await LogTable.find()
+        let consumptionList = []
+        let lastConsumption = 0;
+
+        totalConsumption.forEach((item, i) => {
+            
+            // User's current consumption
+            let consumption = item.total_energy - lastConsumption 
+            
+            if(item.username === req.params.username) {
+                console.log("Total:" + item.username + " = " + consumption);
+                energyObj = {
+                    username: item.username,
+                    consumption: consumption
+                }
+
+                // Append the obj to the list
+                consumptionList.push(energyObj)
+            }
+
+            // Store last user's consumption
+            lastConsumption = item.total_energy
+        });
+
+        const latestEnteries = consumptionList.slice(-3)
+
+        // console.log(energyUsed);
+        res.status(200).json(latestEnteries);
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error');
@@ -27,7 +47,10 @@ exports.getTotalEnergyAllUsers = async (req, res) => {
 
         totalConsumption.forEach((item, i) => {
             
+            // User's current consumption
             let consumption = item.total_energy - lastConsumption 
+            
+            // Find user's obj
             energyObj = energyUsed.find(energyItem => {
                 return item.username === energyItem.username
             })
@@ -35,7 +58,9 @@ exports.getTotalEnergyAllUsers = async (req, res) => {
             // Declare updated obj
             let updatedObj = {}
 
+            // Check and update the obj with consumption
             if(energyObj) {
+                // Update object
                 energyUsed = energyUsed.map(obj => {
                     if(energyObj.username === obj.username){
                         return {
@@ -44,10 +69,11 @@ exports.getTotalEnergyAllUsers = async (req, res) => {
                         }
                     } else {
                         return obj
-                    }
-                        
+                    }                        
                 })
             } else {
+
+                // Return new opject
                 updatedObj = {
                     username: item.username,
                     consumption: consumption
@@ -55,8 +81,8 @@ exports.getTotalEnergyAllUsers = async (req, res) => {
                 energyUsed.push(updatedObj)
             }
             
+            // Store last user's consumption
             lastConsumption = item.total_energy
-            
         });
 
         // console.log(energyUsed);
