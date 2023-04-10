2cb898b008cafa2b9070e68c6f6cd20bbbcd2b8e
Anurag Rawal <anuragr07@yahoo.com>
2023-04-10 02:23:10 -0700

    Added tables for kitchen and laundry

--- Wb-services/web-services/Routes/KitchenTableRoute/KitchenTableRoute.js
+++ Wb-services/web-services/Routes/KitchenTableRoute/KitchenTableRoute.js
@@ -14,6 +14,12 @@ router.get('/total', kitchenTableController.getTotalEnergyAllUsers)
 // POST route to create a new log table entry
 // router.post('/', kitchenTableController.createkitchenTable);
 
+// POST request to turn off kitchen
+router.post('/:username/on', kitchenTableController.turnOnKitchen)
+
+// POST request to turn off kitchen
+router.post('/:username/off', kitchenTableController.turnOffKitchen)
+
 // PUT route to update an existing log table entry
 // router.put('/:id', kitchenTableController.updatekitchenTable);
 
